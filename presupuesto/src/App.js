91ddebdfcefbf8a64fd2163bded538b0d497b1d8
Kev <kevinxpt27@gmail.com>
2020-09-11 19:46:10 -0500

    pres

---
+++ presupuesto/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <header>
+          <h1> Gasto Semanal</h1>
+        </header>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ presupuesto/src/components/Pregunta.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const Pregunta = () => {
+    return ( 
+        <Fragment>
+            <h2> Coloca tu Presupuesto</h2>
+            <form>
+                <input />
+            </form>
+        </Fragment>
+     );
+}
+ 
+export default Pregunta;
