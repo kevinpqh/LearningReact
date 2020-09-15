c1c233b16da2d917ee3c816cb7d03e1f970b7f83
Kev <kevinxpt27@gmail.com>
2020-09-14 23:07:30 -0500

    fin project clima

--- clima/README.md
+++ clima/README.md
@@ -1,3 +1,4 @@
 # Project Clima
 1. usa esta api
 [openweathermap](https://openweathermap.org/)
+2. cambiar el key de openweather
--- clima/src/App.js
+++ clima/src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/Header'
+import Clima from './components/Clima'
+import Error from './components/Error'
 
 function App() {
+
+  const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
+  const [error, setError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+      if (consultar) {
+
+        const appId = '8ed21d3b60318e1d24b1e35cfa72';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const res = await fetch(url)
+        const result = await res.json()
+
+        setResultado(result);
+        setConsultar(false);
+
+        if (result.cod === "404") {
+          setError(true);
+        }
+        else {
+          setError(false);
+        }
+      }
+
+    }
+    consultarAPI();
+
+    // eslint-disable-next-line
+  }, [consultar]);
+
+
+  let componente;
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima
+      resultado={resultado}
+    />
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Clima React App"></Header>
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+              {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </Fragment>
   );
 }
 
