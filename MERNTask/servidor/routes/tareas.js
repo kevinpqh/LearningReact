07551dfa67a2f37a3b01a1811db32c35ac38b68c
Kevin <kevinxpt27@gmail.com>
2020-09-23 09:59:29 -0500

    fix

--- MERNTask/servidor/routes/tareas.js
+++ MERNTask/servidor/routes/tareas.js
@@ -14,4 +14,22 @@ router.post('/',
     tareaController.crearTarea
 );
 
+// Obtener las tareas por proyecto
+router.get('/',
+    auth,
+    tareaController.obtenerTareas
+);
+
+// Actualizar tarea
+router.put('/:id', 
+    auth,
+    tareaController.actualizarTarea
+);
+
+// Eliminar tarea
+router.delete('/:id', 
+    auth,
+    tareaController.eliminarTarea
+);
+
 module.exports = router;
