0f7713dc8715c69352c580c3089163066088eaaf
Kevin <kevinxpt27@gmail.com>
2020-09-29 23:34:01 -0500

    configurando redux

---
+++ crudredux/src/components/NuevoProducto.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+//actions
+import { crearNuevoProductoAction } from '../actions/productoActions';
+
+const NuevoProducto = () => {
+
+  // state del componente
+  const [nombre, setNombre] = useState('');
+  const [precio, setPrecio] = useState(0);
+
+  // utilizar use dispatch y te crea una función
+  const dispatch = useDispatch();
+
+  const agregarProducto = producto => dispatch(crearNuevoProductoAction(producto));
+
+  const submitNuevoProducto = e => {
+    e.preventDefault();
+
+    if(nombre.trim() === '' || precio <= 0) {
+
+      // const alerta = {
+      //     msg: 'Ambos campos son obligatorios',
+      //     classes: 'alert alert-danger text-center text-uppercase p3'
+      // }
+      // dispatch( mostrarAlerta(alerta) );
+
+      return;
+  }
+
+    agregarProducto({
+      nombre,
+      precio
+    });
+  }
+
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+
+            <form onSubmit={submitNuevoProducto}>
+              <div className="form-group">
+                <label>Nombre Producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                  value={nombre}
+                  onChange={e => setNombre(e.target.value)}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Precio Producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio Producto"
+                  name="precio"
+                  value={precio}
+                  onChange={e => setPrecio(Number(e.target.value))}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >Agregar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NuevoProducto;
