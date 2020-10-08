a237e545fff392061dceffc29c2b562633ca0252
Kevin <kevinxpt27@gmail.com>
2020-10-08 00:24:21 -0500

    detalle producto

--- producthuntnext/pages/index.js
+++ producthuntnext/pages/index.js
@@ -1,10 +1,25 @@
+import DetallesProducto from '../components/layout/DetallesProducto';
 import Layout from '../components/layout/Layout';
 
 const Home = () => {
+  
+  const productos = [];
+
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map(producto => (
+                <DetallesProducto
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   )
