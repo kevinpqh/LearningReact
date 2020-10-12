d62ba26ac0b2d4dc5f0b28e56077729c32d3e015
Kevin <kevinxpt27@gmail.com>
2020-10-11 19:26:56 -0500

    init project gatsby

---
+++ hotelgatsby/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ImagenHotel from "../components/imagenHotel";
+import Layout from '../components/layout';
+
+
+const IndexPage = () => (
+  <Layout>
+    <ImagenHotel></ImagenHotel>
+  </Layout>
+)
+
+export default IndexPage
