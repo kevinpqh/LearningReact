02c326d542041c35bf53dac419076b7ce894b519
Kevin <kevinxpt27@gmail.com>
2020-10-16 22:51:19 -0500

    inicio y nosotros

--- hotelgatsby/src/pages/index.js
+++ hotelgatsby/src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import ImagenHotel from "../components/imagenHotel";
 import Layout from '../components/layout';
-
+import ContenidoInicio from "../components/contenidoInicio";
 
 const IndexPage = () => (
   <Layout>
     <ImagenHotel></ImagenHotel>
+    <ContenidoInicio />
   </Layout>
 )
 
